8b460311a5aac6be114edb5beaab15cab6330194
AndreiLacatos <andrei_lacatos@yahoo.com>
2025-01-18 20:00:08 +0200

    Added more unit tests

--- src/node-engine/engine_impl_test.go
+++ src/node-engine/engine_impl_test.go
@@ -2,6 +2,11 @@ package nodeengine_test
 
 import (
 	"context"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"path"
+	"strconv"
 	"testing"
 	"time"
 
@@ -11,6 +16,7 @@ import (
 	"github.com/AndreiLacatos/opc-engine/node-engine/models/waveform"
 	waveformvalue "github.com/AndreiLacatos/opc-engine/node-engine/models/waveform/waveform_value"
 	"github.com/google/uuid"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 )
 
@@ -139,35 +145,179 @@ func TestSingleBooleanNodeValues_TransitionEvery200Ms_SingleCycle(t *testing.T) 
 
 	// act
 	testStart := time.Now()
-	nodeSamples := c.CollectSamples(context.TODO(), e, time.Duration(n.Waveform.Duration)*time.Millisecond)
+	nodeSamples := c.CollectSamples(context.TODO(), e, time.Duration(int(float32(n.Waveform.Duration)*0.95))*time.Millisecond)
 
 	// assert
-	booleanSamples := nodeSamples[n.Id]
-
-	// expect one sample for each transition point & an extra one for the very
-	// first tick (at tick 0 as soon as the engine boots it emits the starting value)
-	expectedSampleCount := len(n.Waveform.TransitionPoints) + 1
-	expectedTransitionTimestamps := make([]time.Time, 0, expectedSampleCount)
-	expectedTransitionTimestamps = append(expectedTransitionTimestamps, testStart)
-	for _, t := range n.Waveform.TransitionPoints {
-		delta := time.Duration(t.Tick) * time.Millisecond
-		expectedTransitionTimestamps = append(expectedTransitionTimestamps, testStart.Add(delta))
-	}
-
-	if len(booleanSamples.samples) != expectedSampleCount {
-		t.Errorf("expected %d samples and got %d", expectedSampleCount, len(booleanSamples.samples))
+	booleanSamples := nodeSamples[n.Id].samples
+	expectedSamples, err := loadExpectedBooleanResultSet("boolean test data.csv")
+	if err != nil {
+		t.Errorf("could not load expected test results: %v", err)
 		t.FailNow()
 	}
 
-	sampleSet := nodeSamples[n.Id].samples
+	wiggle := time.Duration(15) * time.Millisecond
+	adjustExpectedTimestamps(&expectedSamples, testStart)
+	printSamples(l, expectedSamples.samples, testStart)
+	printSamples(l, booleanSamples, testStart)
+	assertBooleanSamplesets(t, expectedSamples.samples, booleanSamples, wiggle)
+}
+
+func TestSingleBooleanNodeValues_TransitionEvery137Ms_WaveformDuration1700Ms_CollectionDuration4420Ms(t *testing.T) {
+	// arrange
+	l := zaptest.NewLogger(t)
+	n := &opcnode.OpcValueNode{
+		Id:    uuid.MustParse("da858518-50c9-4e55-b312-6370275b412d"),
+		Label: "Boolean",
+		Waveform: waveform.Waveform{
+			Duration:      1700,
+			TickFrequency: 137,
+			WaveformType:  waveform.Transitions,
+			TransitionPoints: []waveform.WaveformValue{
+				{
+					Tick:  137,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  274,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  411,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  548,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  685,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  822,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  959,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  1096,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  1233,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  1370,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  1507,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  1644,
+					Value: &waveformvalue.Transition{},
+				},
+			},
+		},
+	}
+	s := opc.OpcStructure{
+		Root: opcnode.OpcContainerNode{
+			Id:    uuid.New(),
+			Label: "Root",
+			Children: []opcnode.OpcStructureNode{
+				n,
+			},
+		},
+	}
+	e := nodeengine.CreateNew(s, l, false)
+	c := SampleCollector{}
+
+	// act
+	testStart := time.Now()
+	nodeSamples := c.CollectSamples(context.TODO(), e, time.Duration(4420)*time.Millisecond)
+
+	// assert
+	booleanSamples := nodeSamples[n.Id].samples
+	expectedSamples, err := loadExpectedBooleanResultSet("boolean test data 2.csv")
+	if err != nil {
+		t.Errorf("could not load expected test results: %v", err)
+		t.FailNow()
+	}
+
+	wiggle := time.Duration(20) * time.Millisecond
+	adjustExpectedTimestamps(&expectedSamples, testStart)
+	printSamples(l, expectedSamples.samples, testStart)
+	printSamples(l, booleanSamples, testStart)
+	assertBooleanSamplesets(t, expectedSamples.samples, booleanSamples, wiggle)
+}
+
+func TestSingleBooleanNodeValues_TransitionsRandomly_WaveformDuration950Ms_CollectionDuration6140Ms(t *testing.T) {
+	// arrange
+	l := zaptest.NewLogger(t)
+	n := &opcnode.OpcValueNode{
+		Id:    uuid.MustParse("da858518-50c9-4e55-b312-6370275b412d"),
+		Label: "Boolean",
+		Waveform: waveform.Waveform{
+			Duration:      1300,
+			TickFrequency: 75,
+			WaveformType:  waveform.Transitions,
+			TransitionPoints: []waveform.WaveformValue{
+				{
+					Tick:  150,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  225,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  450,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  825,
+					Value: &waveformvalue.Transition{},
+				},
+				{
+					Tick:  900,
+					Value: &waveformvalue.Transition{},
+				},
+			},
+		},
+	}
+	s := opc.OpcStructure{
+		Root: opcnode.OpcContainerNode{
+			Id:    uuid.New(),
+			Label: "Root",
+			Children: []opcnode.OpcStructureNode{
+				n,
+			},
+		},
+	}
+	e := nodeengine.CreateNew(s, l, false)
+	c := SampleCollector{}
+
+	// act
+	testStart := time.Now()
+	nodeSamples := c.CollectSamples(context.TODO(), e, time.Duration(6140)*time.Millisecond)
+
+	// assert
+	booleanSamples := nodeSamples[n.Id].samples
+	expectedSamples, err := loadExpectedBooleanResultSet("boolean test data 3.csv")
+	if err != nil {
+		t.Errorf("could not load expected test results: %v", err)
+		t.FailNow()
+	}
 
 	wiggle := time.Duration(15) * time.Millisecond
-	for i, e := range expectedTransitionTimestamps {
-		if !areClose(sampleSet[i].timestamp, e, wiggle) {
-			t.Errorf("expected sample %d to take place on %s, actual: %s", i+1, formatDate(e), formatDate(sampleSet[i].timestamp))
-			t.FailNow()
-		}
-	}
+	adjustExpectedTimestamps(&expectedSamples, testStart)
+	printSamples(l, expectedSamples.samples, testStart)
+	printSamples(l, booleanSamples, testStart)
+	assertBooleanSamplesets(t, expectedSamples.samples, booleanSamples, wiggle)
 }
 
 func areClose(t1, t2 time.Time, wiggleRoom time.Duration) bool {
@@ -177,4 +327,85 @@ func areClose(t1, t2 time.Time, wiggleRoom time.Duration) bool {
 
 func formatDate(t time.Time) string {
 	return t.Format("2006-01-02 15:04:05.999")
+}
+
+func loadExpectedBooleanResultSet(f string) (ResultSet, error) {
+	r := ResultSet{
+		samples: make([]Sample, 0),
+	}
+
+	file, err := os.Open(path.Join("..", "..", "test data", f))
+	if err != nil {
+		return r, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return r, err
+	}
+
+	for i, row := range rows {
+		if i == 0 {
+			continue
+		}
+		if len(row) != 2 {
+			continue
+		}
+
+		number, err := strconv.Atoi(row[0])
+		if err != nil {
+			continue
+		}
+
+		val, err := strconv.ParseBool(row[1])
+		if err != nil {
+			continue
+		}
+
+		var t time.Time
+		r.samples = append(r.samples, Sample{
+			timestamp: t.Add(time.Duration(number) * time.Millisecond),
+			value: &waveformvalue.Transition{
+				Value: val,
+			},
+		})
+	}
+
+	return r, nil
+}
+
+func adjustExpectedTimestamps(r *ResultSet, o time.Time) {
+	var t time.Time
+	for i := range r.samples {
+		delta := r.samples[i].timestamp.Sub(t)
+		r.samples[i].timestamp = o.Add(delta)
+	}
+}
+
+func assertBooleanSamplesets(t *testing.T, expected []Sample, actual []Sample, wiggle time.Duration) {
+	if len(actual) != len(expected) {
+		t.Errorf("expected %d samples and got %d", len(expected), len(actual))
+		t.FailNow()
+	}
+
+	for i, e := range expected {
+		if !areClose(actual[i].timestamp, e.timestamp, wiggle) {
+			t.Errorf("expected sample %d to take place on %s, actual: %s", i+1, formatDate(e.timestamp), formatDate(actual[i].timestamp))
+			t.FailNow()
+		}
+		if actual[i].value.GetValue() != e.value.GetValue() {
+			t.Errorf("at %s expected value %v, actual: %s", formatDate(e.timestamp), e.value.GetValue(), actual[i].value.GetValue())
+			t.FailNow()
+		}
+	}
+}
+
+func printSamples(l *zap.Logger, s []Sample, o time.Time) {
+	l.Info("Samples:")
+	for _, e := range s {
+		l.Info(fmt.Sprintf("%s,%d,%v", formatDate(e.timestamp), e.timestamp.Sub(o)/1e6, e.value.GetValue()))
+	}
 }
