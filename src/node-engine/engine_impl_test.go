f6672ea37cac959699a1390ff305271a5764b3be
AndreiLacatos <andrei_lacatos@yahoo.com>
2025-01-19 10:30:34 +0200

    Added numeric waveform tests

--- src/node-engine/engine_impl_test.go
+++ src/node-engine/engine_impl_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/csv"
 	"fmt"
+	"math"
 	"os"
 	"path"
 	"strconv"
@@ -320,6 +321,279 @@ func TestSingleBooleanNodeValues_TransitionsRandomly_WaveformDuration1300Ms_Coll
 	assertBooleanSamplesets(t, expectedSamples.samples, booleanSamples, wiggle)
 }
 
+func TestSingleNumericNodeValues_StepSmoothing_WaveformDuration2700Ms_CollectionDuration6185Ms(t *testing.T) {
+	// arrange
+	l := zaptest.NewLogger(t)
+	var m waveform.WaveformMeta = waveform.NumericWaveformMeta{
+		Smoothing: waveform.Step,
+	}
+	n := &opcnode.OpcValueNode{
+		Id:    uuid.MustParse("da858518-50c9-4e55-b312-6370275b412d"),
+		Label: "Numbers",
+		Waveform: waveform.Waveform{
+			Duration:      2700,
+			TickFrequency: 50,
+			WaveformType:  waveform.NumericValues,
+			Meta:          &m,
+			TransitionPoints: []waveform.WaveformValue{
+				{
+					Tick: 250,
+					Value: &waveformvalue.DoubleValue{
+						Value: 40.0,
+					},
+				},
+				{
+					Tick: 500,
+					Value: &waveformvalue.DoubleValue{
+						Value: 18.42,
+					},
+				},
+				{
+					Tick: 1000,
+					Value: &waveformvalue.DoubleValue{
+						Value: -14.75,
+					},
+				},
+				{
+					Tick: 1500,
+					Value: &waveformvalue.DoubleValue{
+						Value: 0.17,
+					},
+				},
+				{
+					Tick: 1650,
+					Value: &waveformvalue.DoubleValue{
+						Value: 10.57,
+					},
+				},
+				{
+					Tick: 2250,
+					Value: &waveformvalue.DoubleValue{
+						Value: 4.8,
+					},
+				},
+				{
+					Tick: 2400,
+					Value: &waveformvalue.DoubleValue{
+						Value: 69.02,
+					},
+				},
+			},
+		},
+	}
+	s := opc.OpcStructure{
+		Root: opcnode.OpcContainerNode{
+			Id:    uuid.New(),
+			Label: "Root",
+			Children: []opcnode.OpcStructureNode{
+				n,
+			},
+		},
+	}
+	e := nodeengine.CreateNew(s, l, false)
+	c := SampleCollector{}
+
+	// act
+	testStart := time.Now()
+	nodeSamples := c.CollectSamples(context.TODO(), e, time.Duration(6185)*time.Millisecond)
+
+	// assert
+	numericSamples := nodeSamples[n.Id].samples
+	expectedSamples, err := loadExpectedNumericResultSet("numeric values - steps.csv")
+	if err != nil {
+		t.Errorf("could not load expected test results: %v", err)
+		t.FailNow()
+	}
+
+	wiggle := time.Duration(35) * time.Millisecond
+	adjustExpectedTimestamps(&expectedSamples, testStart)
+	printSamples(l, expectedSamples.samples, testStart)
+	printSamples(l, numericSamples, testStart)
+	assertNumericSamplesets(t, expectedSamples.samples, numericSamples, wiggle)
+}
+
+func TestSingleNumericNodeValues_LinearSmoothing_WaveformDuration2700Ms_CollectionDuration6185Ms(t *testing.T) {
+	// arrange
+	l := zaptest.NewLogger(t)
+	var m waveform.WaveformMeta = waveform.NumericWaveformMeta{
+		Smoothing: waveform.Linear,
+	}
+	n := &opcnode.OpcValueNode{
+		Id:    uuid.MustParse("da858518-50c9-4e55-b312-6370275b412d"),
+		Label: "Numbers",
+		Waveform: waveform.Waveform{
+			Duration:      2700,
+			TickFrequency: 50,
+			WaveformType:  waveform.NumericValues,
+			Meta:          &m,
+			TransitionPoints: []waveform.WaveformValue{
+				{
+					Tick: 250,
+					Value: &waveformvalue.DoubleValue{
+						Value: 40.0,
+					},
+				},
+				{
+					Tick: 500,
+					Value: &waveformvalue.DoubleValue{
+						Value: 18.42,
+					},
+				},
+				{
+					Tick: 1000,
+					Value: &waveformvalue.DoubleValue{
+						Value: -14.75,
+					},
+				},
+				{
+					Tick: 1500,
+					Value: &waveformvalue.DoubleValue{
+						Value: 0.17,
+					},
+				},
+				{
+					Tick: 1650,
+					Value: &waveformvalue.DoubleValue{
+						Value: 10.57,
+					},
+				},
+				{
+					Tick: 2250,
+					Value: &waveformvalue.DoubleValue{
+						Value: 4.8,
+					},
+				},
+				{
+					Tick: 2400,
+					Value: &waveformvalue.DoubleValue{
+						Value: 69.02,
+					},
+				},
+			},
+		},
+	}
+	s := opc.OpcStructure{
+		Root: opcnode.OpcContainerNode{
+			Id:    uuid.New(),
+			Label: "Root",
+			Children: []opcnode.OpcStructureNode{
+				n,
+			},
+		},
+	}
+	e := nodeengine.CreateNew(s, l, false)
+	c := SampleCollector{}
+
+	// act
+	testStart := time.Now()
+	nodeSamples := c.CollectSamples(context.TODO(), e, time.Duration(6185)*time.Millisecond)
+
+	// assert
+	numericSamples := nodeSamples[n.Id].samples
+	expectedSamples, err := loadExpectedNumericResultSet("numeric values - linear.csv")
+	if err != nil {
+		t.Errorf("could not load expected test results: %v", err)
+		t.FailNow()
+	}
+
+	wiggle := time.Duration(35) * time.Millisecond
+	adjustExpectedTimestamps(&expectedSamples, testStart)
+	printSamples(l, expectedSamples.samples, testStart)
+	printSamples(l, numericSamples, testStart)
+	assertNumericSamplesets(t, expectedSamples.samples, numericSamples, wiggle)
+}
+
+func TestSingleNumericNodeValues_CubicSplineSmoothing_WaveformDuration2700Ms_CollectionDuration6185Ms(t *testing.T) {
+	// arrange
+	l := zaptest.NewLogger(t)
+	var m waveform.WaveformMeta = waveform.NumericWaveformMeta{
+		Smoothing: waveform.CubicSpline,
+	}
+	n := &opcnode.OpcValueNode{
+		Id:    uuid.MustParse("da858518-50c9-4e55-b312-6370275b412d"),
+		Label: "Numbers",
+		Waveform: waveform.Waveform{
+			Duration:      2700,
+			TickFrequency: 50,
+			WaveformType:  waveform.NumericValues,
+			Meta:          &m,
+			TransitionPoints: []waveform.WaveformValue{
+				{
+					Tick: 250,
+					Value: &waveformvalue.DoubleValue{
+						Value: 40.0,
+					},
+				},
+				{
+					Tick: 500,
+					Value: &waveformvalue.DoubleValue{
+						Value: 18.42,
+					},
+				},
+				{
+					Tick: 1000,
+					Value: &waveformvalue.DoubleValue{
+						Value: -14.75,
+					},
+				},
+				{
+					Tick: 1500,
+					Value: &waveformvalue.DoubleValue{
+						Value: 0.17,
+					},
+				},
+				{
+					Tick: 1650,
+					Value: &waveformvalue.DoubleValue{
+						Value: 10.57,
+					},
+				},
+				{
+					Tick: 2250,
+					Value: &waveformvalue.DoubleValue{
+						Value: 4.8,
+					},
+				},
+				{
+					Tick: 2400,
+					Value: &waveformvalue.DoubleValue{
+						Value: 69.02,
+					},
+				},
+			},
+		},
+	}
+	s := opc.OpcStructure{
+		Root: opcnode.OpcContainerNode{
+			Id:    uuid.New(),
+			Label: "Root",
+			Children: []opcnode.OpcStructureNode{
+				n,
+			},
+		},
+	}
+	e := nodeengine.CreateNew(s, l, false)
+	c := SampleCollector{}
+
+	// act
+	testStart := time.Now()
+	nodeSamples := c.CollectSamples(context.TODO(), e, time.Duration(6185)*time.Millisecond)
+
+	// assert
+	numericSamples := nodeSamples[n.Id].samples
+	expectedSamples, err := loadExpectedNumericResultSet("numeric values - cubic spline.csv")
+	if err != nil {
+		t.Errorf("could not load expected test results: %v", err)
+		t.FailNow()
+	}
+
+	wiggle := time.Duration(35) * time.Millisecond
+	adjustExpectedTimestamps(&expectedSamples, testStart)
+	printSamples(l, expectedSamples.samples, testStart)
+	printSamples(l, numericSamples, testStart)
+	assertNumericSamplesets(t, expectedSamples.samples, numericSamples, wiggle)
+}
+
 func areClose(t1, t2 time.Time, wiggleRoom time.Duration) bool {
 	diff := t1.Sub(t2)
 	return diff <= wiggleRoom && diff >= -wiggleRoom
@@ -377,6 +651,54 @@ func loadExpectedBooleanResultSet(f string) (ResultSet, error) {
 	return r, nil
 }
 
+func loadExpectedNumericResultSet(f string) (ResultSet, error) {
+	r := ResultSet{
+		samples: make([]Sample, 0),
+	}
+
+	file, err := os.Open(path.Join("..", "..", "test data", f))
+	if err != nil {
+		return r, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	rows, err := reader.ReadAll()
+	if err != nil {
+		return r, err
+	}
+
+	for i, row := range rows {
+		if i == 0 {
+			continue
+		}
+		if len(row) != 2 {
+			continue
+		}
+
+		number, err := strconv.Atoi(row[0])
+		if err != nil {
+			continue
+		}
+
+		val, err := strconv.ParseFloat(row[1], 64)
+		if err != nil {
+			continue
+		}
+
+		var t time.Time
+		r.samples = append(r.samples, Sample{
+			timestamp: t.Add(time.Duration(number) * time.Millisecond),
+			value: &waveformvalue.DoubleValue{
+				Value: val,
+			},
+		})
+	}
+
+	return r, nil
+}
+
 func adjustExpectedTimestamps(r *ResultSet, o time.Time) {
 	var t time.Time
 	for i := range r.samples {
@@ -403,8 +725,29 @@ func assertBooleanSamplesets(t *testing.T, expected []Sample, actual []Sample, w
 	}
 }
 
+func assertNumericSamplesets(t *testing.T, expected []Sample, actual []Sample, wiggle time.Duration) {
+	if len(actual) != len(expected) {
+		t.Errorf("expected %d samples and got %d", len(expected), len(actual))
+		t.FailNow()
+	}
+
+	for i, e := range expected {
+		if !areClose(actual[i].timestamp, e.timestamp, wiggle) {
+			t.Errorf("expected sample %d to take place on %s, actual: %s", i+1, formatDate(e.timestamp), formatDate(actual[i].timestamp))
+			t.FailNow()
+		}
+		expectedValue := e.value.GetValue().(float64)
+		actualValue := actual[i].value.GetValue().(float64)
+		diff := expectedValue - actualValue
+		if math.Abs(diff) > 0.02 {
+			t.Errorf("at %s expected value %v, actual: %s", formatDate(e.timestamp), e.value.GetValue(), actual[i].value.GetValue())
+			t.FailNow()
+		}
+	}
+}
+
 func printSamples(l *zap.Logger, s []Sample, o time.Time) {
-	l.Info("Samples:")
+	l.Info("Samples (time,tick,value):")
 	for _, e := range s {
 		l.Info(fmt.Sprintf("%s,%d,%v", formatDate(e.timestamp), e.timestamp.Sub(o)/1e6, e.value.GetValue()))
 	}
